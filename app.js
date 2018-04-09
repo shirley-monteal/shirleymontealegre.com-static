4cf67c08948ea490fbb57132b48e6605ed9e0a28
Shirley M <smontealegre01@hotmail.com>
2018-04-09 13:28:29 -0400

    email input

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 app.use('/', express.static(__dirname + '/static_html'));
 app.listen(8080, '172.31.22.173'); // ip address is internal aws
+
+//Note that in version 4 of express, express.bodyParser() was
+//deprecated in favor of a separate 'body-parser' module.
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+//app.use(express.bodyParser());
+
+app.post('/contact', function(req, res) {
+  res.send('Thank you!! Your email has been sent');
+});
